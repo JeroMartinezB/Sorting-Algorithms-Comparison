27b0ea9e8a34e86ffb73c78e94e7799f1a2e0b69
Jeronimo Martinez Barragan <157544777+jmb06@users.noreply.github.com>
2024-12-28 15:19:30 -0500

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,27 @@
+// Insertion Sort algorithm
+// Version adapted from Sedgewick, R. Wayne, K.
+// Algorithms
+// https://www.geeksforgeeks.org/insertion-sort-algorithm/
+
+public class InsertionSort{
+
+    public void sort(int[]numbers){
+
+        int n = numbers.length;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            int key = numbers[i];
+            int j = i -1;
+            //Move elements that are greater than the key 
+            //one position ahead
+            while (j>=0 && numbers[j]>key){
+                numbers[j+1] = numbers[j];
+                j--;
+            }
+            numbers[j+1] = key;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Execution time for Insertion Sort: " + (end-start) + " ms.");
+    }
+
+}
